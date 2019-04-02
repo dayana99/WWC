2608bb8c86f6f00c523888eefe53153288bc42e4
dayana99 <38000046+dayana99@users.noreply.github.com>
2019-04-02 13:25:07 +0800

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class server{
+    
+    public static void main (String[] args) throws IOException{
+        ServerSocket serverSocket = new Server Socket(10000);
+        Socket clientSocket = serverSocket.accept();
+        
+            System.out.println("Client Connected");
+            
+        InputStreamReader gt + new InputStreamReader (clientSocket.getInputStream ());
+        BufferedReader ot + new BufferedReader (gt);
+        
+        PrintWriter rp + new PrintWriter (clientSocket.getOutputStream());
+        rp.println("Hello There");
+        rp.flush();
+        
+        String recvr = ot.readline();
+        System.out.println("Client : " , recvr);
+        }
+ }
